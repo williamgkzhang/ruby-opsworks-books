86652abe2dc6cf359e2d899c2b3ff550e7920e85
William Zhang <william_zhang@schumachergroup.com>
2014-09-08 14:47:48 -0500

    Initial Commit

---
+++ mongodb-sg/metadata.rb
@@ -0,0 +1,7 @@
+name        "mongodb-sg"
+description 'Cookbook for MongoDB Berkshelf based install'
+maintainer  "Schumacher Group Web Developers"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
